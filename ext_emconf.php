800966f9f321b7af007154d6a467e04587096192
Franz Holzinger <holzinger@ttproducts.de>
2019-11-28 13:34:39 +0100

    next version 0.3.0 which also runs under TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,43 +2,29 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "import"
-#
-# Auto generated 18-04-2009 13:03
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Import of data into database tables',
-	'description' => 'This helps you to import data from text files into the tables of TYPO3 or its extensions.',
-	'category' => 'module',
-	'author' => 'Franz Holzinger',
-	'author_email' => 'franz@ttproducts.de',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'alpha',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author_company' => '',
-	'version' => '0.2.0',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.5.0-7.99.99',
-			'typo3' => '7.6.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Import of data into database tables',
+    'description' => 'This helps you to import data from text files into the tables of TYPO3 or its extensions.',
+    'category' => 'module',
+    'author' => 'Franz Holzinger',
+    'author_email' => 'franz@ttproducts.de',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author_company' => '',
+    'version' => '0.3.0',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.5.0-7.99.99',
+            'typo3' => '7.6.0-9.99.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
 
