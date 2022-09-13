4938b08508d13f87d9fd1bb4d030d30e07596e43
Franz Holzinger <franz.holzinger@festwein.de>
2022-09-13 21:55:52 +0200

    composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 # Extension Manager/Repository config file for ext: "import"
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Import of data into database tables',
     'description' => 'This helps you to import data from text files or tables into the tables of TYPO3 or extensions.',
     'category' => 'module',
@@ -16,17 +16,17 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'author_company' => '',
     'version' => '0.5.0',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '8.7.0-9.5.99',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
             'func' => '7.6.0-9.99.99',
             'scheduler' => '7.6.0-9.99.99',
             "typo3db_legacy" => ''
-        ),
-    ),
-);
+        ],
+    ],
+];
 
